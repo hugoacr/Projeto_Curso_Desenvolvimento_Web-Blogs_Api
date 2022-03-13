41072dfe823ec3a71dc8268f579f1e4e1aae73bc
Hugoacr <hugoacr@yahoo.com.br>
2022-03-13 10:42:34 -0300

    corrige json da response

--- services/loginServices.js
+++ services/loginServices.js
@@ -4,7 +4,7 @@ const { User } = require('../models');
 const createNewLogin = async (email, password) => {
   const user = await User.findOne({ where: { email } });
   if (!user || password !== user.password) {
-    return { code: 400, response: { error: 'Invalid fields' } }; 
+    return { code: 400, response: { message: 'Invalid fields' } }; 
   }
   const token = jwtGenerator({ id: user.id, email });
   return { code: 200, response: { token } };

62887328f43c79ae715ff89b37e465d4581cefd3
Hugoacr <hugoacr@yahoo.com.br>
2022-03-13 11:17:11 -0300

    corrige json do response

--- middlewares/authorizationToken.js
+++ middlewares/authorizationToken.js
@@ -4,7 +4,7 @@ module.exports = async (req, res, next) => {
   try {
     const token = req.headers.authorization;
 
-    if (!token) return res.status(401).json({ error: 'Token not found' });
+    if (!token) return res.status(401).json({ message: 'Token not found' });
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
